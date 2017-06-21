ca238a1daf0d515170a09323c172b428c4aea649
Stephane Bellity <stephane@hull.io>
2017-06-21 21:18:13 +0200

    WIP Add mysql adapter - needs more testing

--- server/lib/adapters/index.js
+++ server/lib/adapters/index.js
@@ -1,7 +1,9 @@
 import * as postgres from "./postgres";
+import * as mysql from "./mysql";
 import * as filesystem from "./filesystem";
 import * as s3 from "./s3";
 
+export { mysql };
 export { postgres };
 export { filesystem };
 export { s3 };
