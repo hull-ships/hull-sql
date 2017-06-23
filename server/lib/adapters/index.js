a880ad933683ae57f49c2db7246f4fff0a4707d9
Romain Dardour <romain@hull.io>
2017-06-23 12:37:11 +0200

    support "redshift" type - === postgres.

--- server/lib/adapters/index.js
+++ server/lib/adapters/index.js
@@ -3,7 +3,10 @@ import * as mysql from "./mysql";
 import * as filesystem from "./filesystem";
 import * as s3 from "./s3";
 
+const redshift = postgres;
+
 export { mysql };
+export { redshift };
 export { postgres };
 export { filesystem };
 export { s3 };
