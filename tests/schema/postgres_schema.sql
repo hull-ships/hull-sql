045a32974ad2bafdddfc4d2c68bb47072bfc5e60
Michal Raczka <michal@raczka.me>
2017-07-04 15:54:20 +0200

    Adds support for jobs progress tracking

--- tests/schema/postgres_schema.sql
+++ tests/schema/postgres_schema.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS users_view;
+DROP VIEW IF EXISTS dummy_view;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -28,3 +29,5 @@ CREATE VIEW dummy_view AS SELECT generate_series as external_id FROM generate_se
 -- SELECT id as external_id, name as first_name, foo_numeric as testing_trait, foo_date FROM users_view
 
 -- SELECT id as external_id, name as first_name, foo_numeric as testing_trait, foo_date FROM users_view WHERE foo_date >= :import_start_date
+
+-- SELECT * FROM dummy_view
