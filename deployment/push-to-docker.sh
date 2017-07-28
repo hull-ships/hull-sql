69ad8d2da0830a7b6cfcfdb06dbc7235848c5c90
bdahon <benjamin.dahon@gmail.com>
2017-07-28 16:55:08 +0200

    Removed base64

--- deployment/push-to-docker.sh
+++ deployment/push-to-docker.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-DOCKER_USER_DECODED=`echo $DOCKER_USER | base64 -d`
-DOCKER_PASS_DECODED=`echo $DOCKER_PASS | base64 -d`
-docker login -u $DOCKER_USER_DECODED -p $DOCKER_PASS_DECODED
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker -D push hull/hull-sql:$CIRCLE_SHA1
