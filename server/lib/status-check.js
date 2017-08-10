e8e436dd2654cac829b6d8986a41563e3117c4d9
mkawalek <michalkawalekdevkit@icloud.com>
2017-08-10 12:26:38 +0200

    minor improvements

--- server/lib/status-check.js
+++ server/lib/status-check.js
@@ -6,7 +6,7 @@ import SyncAgent from "./sync-agent";
 
 export default function (req: Request, res: Response) {
   let { agent } = req;
-  const { hull = {} } = req;
+  const { client = {}, ship = {} } = req.hull;
   const messages = [];
   const promises = [];
   let status = "ok";
@@ -16,40 +16,36 @@ export default function (req: Request, res: Response) {
     messages.push("Connection string is not configured");
   } else {
     // check connection and response
-    promises.push(req.agent.runQuery("SELECT 1", { timeout: 3000 }).catch(err => {
+    promises.push(agent.runQuery("SELECT 1", { timeout: 3000 }).catch(err => {
       messages.push(`Error when trying to connect with database. ${_.get(err, "message", "")}`);
     }));
   }
 
-  if (!req.agent.isQueryStringConfigured()) {
+  if (!agent.isQueryStringConfigured()) {
     status = "error";
     messages.push("Query string is not configured");
   } else {
-    if (req.hull && req.hull.ship) {
-      agent = new SyncAgent(req.hull);
-      promises.push(agent.runQuery(agent.getQuery(), { limit: 1, timeout: 3000 }).then(result => {
-        if (result.entries && result.entries.length === 0) {
-          messages.push("Database does not return any rows for saved query");
-        }
+    agent = new SyncAgent(req.hull);
+    promises.push(agent.runQuery(agent.getQuery(), { limit: 1, timeout: 3000 }).then(result => {
+      if (result.entries && result.entries.length === 0) {
+        messages.push("Database does not return any rows for saved query");
+      }
 
-        if (result.errors) {
-          messages.push(result.errors);
-        }
-      }).catch(err => {
-        messages.push(`Error when trying to connect with database. ${_.get(err, "message", "")}`);
-      }));
-    } else {
-      messages.push("Hull client is undefined");
-    }
+      if (result.errors) {
+        messages.push(result.errors);
+      }
+    }).catch(err => {
+      messages.push(`Error when trying to connect with database. ${_.get(err, "message", "")}`);
+    }));
   }
 
-  if (_.get(hull, "ship.private_settings.enabled") && _.get(hull, "ship.private_settings.import_days", 0) < 0) {
+  if (_.get(ship, "private_settings.enabled") && _.get(ship, "private_settings.import_days", 0) < 0) {
     status = "error";
     messages.push("Interval syncing is enabled but interval time is less or equal zero.");
   }
 
   return Promise.all(promises).then(() => {
     res.json({ messages: _.uniq(messages), status });
-    return hull.client.put(req.hull.ship.id, { status, status_messages: _.uniq(messages) });
+    return client.put(ship.id, { status, status_messages: _.uniq(messages) });
   });
 }
