345da81456530c2415a1d74f205303e8ec55d2ae
timliuhull <43348784+timliuhull@users.noreply.github.com>
2019-02-26 13:35:41 -0500

    Revert "Revert "Revert "Release/snowflake connector prod rollout"""

--- server/lib/adapters/index.js
+++ server/lib/adapters/index.js
@@ -3,7 +3,6 @@ import * as mysql from "./mysql";
 import * as s3 from "./s3";
 import * as filesystem from "./filesystem";
 import * as mssql from "./mssql";
-import * as snowflake from "./snowflake";
 
 const redshift = postgres;
 
@@ -13,5 +12,3 @@ export { postgres };
 export { s3 };
 export { filesystem };
 export { mssql };
-export { snowflake };
-
