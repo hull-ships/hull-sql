480d78edd94beb3cfa25e3eaa2366b9f2fbe7838
Stephane Bellity <sbellity@gmail.com>
2019-05-24 12:40:08 +0200

    Fix auth and results parsing

--- server/lib/adapters/salesforce.js
+++ server/lib/adapters/salesforce.js
@@ -5,6 +5,7 @@ import jsforce from "jsforce";
 import csv from "csv-stream";
 import Promise from "bluebird";
 import SequelizeUtils from "sequelize/lib/utils";
+import through2 from "through2";
 import validateResultColumns from "./validate-result-columns";
 import _ from "lodash";
 /**
@@ -17,49 +18,19 @@ import _ from "lodash";
  * @param {Object} settings The ship settings.
  *
  */
-export function _openConnection(settings) {
-  const conn = new jsforce.Connection({
-    loginUrl: settings.instance_url
-  });
-  return new Promise((resolve, reject) => {
-    conn.login(settings.username, settings.password + settings.security_token, (err, res) => {
-      if (err) {
-        console.error(err);
-        return reject(err);
-      }
-      return resolve(conn);
-    });
-  })
-}
-
-
 export function openConnection(settings) {
   const params = {
     oauth2 : {
       clientId : process.env.SALESFORCE_CLIENT_ID,
-      clientSecret : process.env.SALESFORCE_CLIENT_ID
+      clientSecret : process.env.SALESFORCE_CLIENT_SECRET
     },
     instanceUrl : settings.instance_url,
     accessToken : settings.access_token,
     refreshToken : settings.refresh_token
   };
 
+  // TODO: Add suport for refreshing tokens
   const conn = new jsforce.Connection(params);
-
-  conn.on("refresh", function(accessToken, res) {
-    // Refresh event will be fired when renewed access token
-    // to store it in your storage for next request
-    console.warn("---------------->>> refreshed !", { accessToken, res });
-  });
-
-  // Alternatively, you can use the callback style request to fetch the refresh token
-  conn.oauth2.refreshToken(settings.refresh_token, (err, results) => {
-    console.warn("------------------------------------> refresh", {
-      err, results
-    });
-  });
-
-  console.warn("SFDC connection params", { settings, params });
 
   return conn;
 }
@@ -113,6 +84,15 @@ export function wrapQuery(sql, replacements) {
   return SequelizeUtils.formatNamedParameters(sql, replacements, "mysql");
 }
 
+function conformRecord(record) {
+  // TODO: check if we want to lowercase the column names here
+  return _.reduce(record, (m,v,k) => {
+    if (k === 'attributes') return m;
+    m[k.toLowerCase()] = v;
+    return m;
+  }, {})
+}
+
 /**
  * Runs the query using the specified client and options.
  * @param client The Salesforce client.
@@ -123,10 +103,10 @@ export function wrapQuery(sql, replacements) {
  */
 export function runQuery(client, query, options = {}) {
   return new Promise((resolve, reject) => {
-    const soqlText = query; //`${query} LIMIT ${options.limit || 100}`;
+    const soqlText = `${query} LIMIT ${options.limit}`;
     client.query(soqlText, (err, result) => {
       if (err) return reject(err);
-      return resolve({ rows: result.records });
+      return resolve({ rows: result.records.map(conformRecord) });
     });
   });
 }
@@ -140,18 +120,23 @@ export function runQuery(client, query, options = {}) {
  * @returns {Promise} A promise object that wraps a stream.
  */
 export function streamQuery(client, query) {
-  // var options = {
-  //     delimiter : '\t', // default is ,
-  //     endLine : '\n', // default is \n,
-  //     columnOffset : 2, // default is 0
-  //     escapeChar : '"', // default is an empty string
-  //     enclosedChar : '"' // default is an empty string
-  // }
+  const options = {
+    escapeChar : '"',
+    enclosedChar : '"'
+  };
 
-  const options = {};
-  var csvStream = csv.createStream(options);
+  const csvStream = csv.createStream(options);
 
   return new Promise((resolve, reject) => {
-    resolve(client.bulk.query(query).stream().pipe(csvStream));
+    const stream = client.bulk
+      .query(query)
+      .stream()
+      .pipe(csvStream)
+      .pipe(through2({ objectMode: true, highWaterMark: 10 }, (row, enc, callback) => {
+        const conformed = conformRecord(row);
+        return callback(null, conformRecord(row));
+      }));
+
+    return resolve(stream);
   });
 }
