bbdea93e4e1a4d2305de3cc316f8d84a8a29faf3
Stephane Bellity <sbellity@ip-192-168-1-34.ec2.internal>
2019-07-02 11:17:52 +0200

    mysql use mariadb in connection string

--- server/lib/adapters/index.js
+++ server/lib/adapters/index.js
@@ -1,5 +1,5 @@
 import * as postgres from "./postgres";
-import * as mysql from "./mariadb";
+import * as mysql from "./mysql";
 import * as s3 from "./s3";
 import * as filesystem from "./filesystem";
 import * as mssql from "./mssql";
