ee3cc509b987d9aab4c04aae32e025aec1eec756
Sven Maschek <sven@gritbase.co>
2019-08-05 19:52:46 +0200

    Add ssh_host to settings

--- server/lib/utils/ssh-tunnel.js
+++ server/lib/utils/ssh-tunnel.js
@@ -1,8 +1,8 @@
-export function getSshTunnelConfig({ ssh_username, db_host, db_port, ssh_private_key }) {
+export function getSshTunnelConfig({ ssh_username, db_host, db_port, ssh_private_key, ssh_host }) {
   return {
     username: ssh_username,
     port: 22,
-    host: db_host,
+    host: ssh_host,
     dstPort: db_port,
     dstHost: db_host,
     privateKey: ssh_private_key
