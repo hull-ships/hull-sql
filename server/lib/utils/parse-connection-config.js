698fc9cd20798e2990d55adc2300d40f2713157b
Sven Maschek <sven@gritbase.co>
2019-08-08 23:33:25 +0200

    Fix reduce on numeric settings for port

--- server/lib/utils/parse-connection-config.js
+++ server/lib/utils/parse-connection-config.js
@@ -4,6 +4,7 @@ export function parseConnectionConfig(settings) {
   const conn = ["type", "host", "port", "name", "user", "password"].reduce((c, key) => {
     let val = settings[`db_${key}`];
     if (key === "type" && val === "redshift") val = "postgres";
+    if (key === "port" && _.isFinite(val)) val = `${val}`;
     if (c && val && val.length > 0) {
       return { ...c,
         [key]: val
